ab7d32bddae79999671516b9f2e0ee5c636bfe2e
daneshvarkakhaki.p <daneshvarkakhaki.p@gmail.com>
2024-03-02 21:31:39 -0500

    Provided data with new bounding boxes

--- mmaction2/projects/ctrgcn/configs/ctrgcn_infact_plus_2d_primitive_new.py
+++ mmaction2/projects/ctrgcn/configs/ctrgcn_infact_plus_2d_primitive_new.py
@@ -6,11 +6,11 @@ custom_imports = dict(imports='models')
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
-        type='CTRGCN', graph_cfg=dict(layout='human3.6m', mode='spatial')),
+        type='CTRGCN', graph_cfg=dict(layout='coco', mode='spatial')),
     cls_head=dict(type='GCNHead', num_classes=5, in_channels=256))
 
 dataset_type = 'PoseDataset'
-ann_file = '../../../Data/InfAct_plus/2d/primitive/InfAct_plus.pkl'
+ann_file = '../../../Data/new_InfAct_plus.pkl'
 train_pipeline = [
     dict(type='PreNormalize2D'),
     dict(type='GenSkeFeat', dataset='coco', feats=['j']),
@@ -78,17 +78,37 @@ test_dataloader = dict(
 val_evaluator = [dict(type='AccMetric')]
 test_evaluator = val_evaluator
 
+num_epochs = 50
+
 train_cfg = dict(
-    type='EpochBasedTrainLoop', max_epochs=16, val_begin=1, val_interval=1)
+    type='EpochBasedTrainLoop', max_epochs=num_epochs, val_begin=1, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
+#param_scheduler = [
+#    dict(
+#        type='CosineAnnealingLR',
+#        eta_min=0,
+#        T_max=num_epochs,
+#        by_epoch=True,
+#        convert_to_iter_based=True)
+#]
+
 param_scheduler = [
     dict(
-        type='CosineAnnealingLR',
-        eta_min=0,
-        T_max=16,
+        type='LinearLR',
+        start_factor=0.1,
         by_epoch=True,
+        begin=0,
+        end=20,
+        convert_to_iter_based=True),
+    dict(
+        type='CosineAnnealingLR',
+        T_max=30,
+        eta_min=0,
+        by_epoch=True,
+        begin=20,
+        end=50,
         convert_to_iter_based=True)
 ]
 
