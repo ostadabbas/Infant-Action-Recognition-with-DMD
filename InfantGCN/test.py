37f31543d8d8f3c102780b791443179989a1acfb
daneshvarkakhaki.p <daneshvarkakhaki.p@gmail.com>
2024-05-07 15:57:04 -0400

    MMake the training and testing files compatible to the new epoch_runner and logger files

--- InfantGCN/test.py
+++ InfantGCN/test.py
@@ -17,45 +17,12 @@ from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, ChainedSchedul
 from net.stgcn import STGCN
 from net.ctrgcn import CTRGCN
 from dataloader import Feeder
-
-import argparse
-
-def get_parsers():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="CTRGCN", help="The model used for recognition", type=str)
-    parser.add_argument("--weights", help="The model used for recognition", type=str)
-    parser.add_argument("--data_path", help="The data used for recognition", type=str)
-    parser.add_argument("--output_folder", help="output folder to save the results", type=str)
-    parser.add_argument("--exp_name", help="name of the experiments", type=str)
-    return parser
-
-def do_epoch(model, dataloader):
-    torch_mode = torch.no_grad
-    model.eval()
-
-    correct_predictions = 0
-    total_predictions = 0
-    all_preds  = []
-    all_gts = []
-    all_feats = []
-    with torch_mode():
-        for batch_X, batch_y in tqdm.tqdm(dataloader):
-            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-            batch_pred, batch_feats = model.extract_features(batch_X)
-
-            _, predicted_labels = torch.max(batch_pred, 1)
-            correct_predictions += (predicted_labels == batch_y).sum().item()
-            total_predictions += batch_y.size(0)
-            all_preds.extend(predicted_labels.cpu().numpy())
-            all_gts.extend(batch_y.cpu().numpy())
-            all_feats.extend(batch_feats.cpu().numpy())
-        accuracy = correct_predictions / total_predictions
-
-        print(f"test acc: {accuracy:.4f}")
-    return accuracy, all_preds, all_gts, all_feats
+from utils.logger import Logger
+from utils.parsers import get_testing_parser
+from epoch_runner import EpochRunner
 
 if __name__ == "__main__":
-    parser = get_parsers()
+    parser = get_testing_parser()
     args = parser.parse_args()
 
     MODEL = args.model
@@ -67,7 +34,6 @@ if __name__ == "__main__":
 
     N_FEATS = 2
 
-    # test_dataset = Feeder(f"../Data/InfAct_plus/InfAct_plus_{N_FEATS}d_yolo.pkl", 'test', window_size=60, random_selection="uniform_choose")
     test_dataset = Feeder(DATA_PATH, 'test', window_size=60, random_selection="uniform_choose", break_samples=False)
     test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
@@ -83,7 +49,8 @@ if __name__ == "__main__":
         model = CTRGCN(input_dim, num_classes, graph_args, **kwargs).to(device)
 
     model.load_state_dict(torch.load(WEIGHTS))
-
-    test_accuracy, preds, gts, feats = do_epoch(model, test_dataloader)
+    test_logger = Logger('train', osp.join(WORK_DIR, 'test_log.txt'))
+    epoch_runner = EpochRunner(model, device, test_logger)
+    test_accuracy, preds, gts, feats = epoch_runner.run_epoch('val', 0, test_dataloader)
     with open(osp.join(WORK_DIR, "eval.pkl"), "wb") as f:
         pickle.dump({"Accuracy": test_accuracy, "pred_label": preds, "gt_label": gts, "feats": feats}, f)
--- InfantGCN/train.py
+++ InfantGCN/train.py
@@ -6,7 +6,6 @@ import math
 import tqdm
 import os
 import os.path as osp
-import glob
 from datetime import datetime
 
 from torch.utils.data import DataLoader
@@ -17,68 +16,14 @@ from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, ChainedSchedul
 from net.stgcn import STGCN
 from net.ctrgcn import CTRGCN
 from dataloader import Feeder
+from utils.logger import Logger
+from utils.training_utils import save_weights
+from utils.parsers import get_training_parser
+from epoch_runner import EpochRunner
 
-import argparse
-
-def get_parsers():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="CTRGCN", help="The model used for recognition", type=str)
-    parser.add_argument("--data_path", help="The data used for recognition", type=str)
-    parser.add_argument("--base_lr", default=0.1, help="Base learing rate", type=float)
-    parser.add_argument("--epochs", default=20, help="number of epochs to train the dataset", type=int)
-    parser.add_argument("--repeat", default=1, help="number of times to repeat training dataset", type=int)
-    parser.add_argument("--output_folder", help="output folder to save the results", type=str)
-    parser.add_argument("--exp_name", help="name of the experiments", type=str)
-    return parser
-
-def save_weights(model, name, rem_prev=None):
-    if rem_prev is not None:
-        pre_results = glob.glob(osp.join(WORK_DIR, f"{rem_prev}*.pth"))
-        assert len(pre_results)<=1
-        if len(pre_results)==1:
-            os.remove(pre_results[0])
-    torch.save(model.state_dict(), osp.join(WORK_DIR, name))
-
-
-def do_epoch(mode, epoch_num, model, dataloader):
-    if mode == 'train':
-        torch_mode = torch.enable_grad
-        model.train()
-        running_loss = 0
-    else:
-        torch_mode = torch.no_grad
-        model.eval()
-
-    correct_predictions = 0
-    total_predictions = 0
-    with torch_mode():
-        for batch_X, batch_y in tqdm.tqdm(dataloader):
-            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-            batch_pred = model(batch_X)
-
-            if mode=='train':
-                loss = ce_loss(batch_pred, batch_y)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                scheduler = scheduler1 if epoch_num<0.4*EPOCHS else scheduler2
-                scheduler.step()
-                running_loss += loss.item()
-
-            _, predicted_labels = torch.max(batch_pred, 1)
-            correct_predictions += (predicted_labels == batch_y).sum().item()
-            total_predictions += batch_y.size(0)
-        accuracy = correct_predictions / total_predictions
-
-        if mode=='train':
-            epoch_loss = running_loss/len(dataloader)
-            print(f"epoch: {epoch_num+1}, train_loss; {epoch_loss:.4f}, train acc: {accuracy:.4f}, lr: {scheduler.get_last_lr()[0]:.5f}")
-        else:
-            print(f"epoch: {epoch_num+1}, {mode} acc: {accuracy:.4f}")
-    return accuracy
 
 if __name__ == "__main__":
-    parser = get_parsers()
+    parser = get_training_parser()
     args = parser.parse_args()
 
     EPOCHS = args.epochs
@@ -91,13 +36,10 @@ if __name__ == "__main__":
     OUT_FOLDER = args.output_folder if args.output_folder is not None else "../Results_test"
     WORK_DIR = osp.join(OUT_FOLDER, EXP_NAME)
     os.makedirs(WORK_DIR, exist_ok=True)
-
     
     N_FEATS = 2
 
-    # train_dataset = Feeder(f"../Data\\InfAct_plus\\InfAct_plus_{N_FEATS}d_yt_split.pkl", 'train', window_size=60, random_selection="uniform_choose", repeat=REPEAT)
     train_dataset = Feeder(DATA_PATH, 'train', window_size=60, random_selection="uniform_choose", repeat=REPEAT, break_samples=False)
-    # val_dataset = Feeder(f"../Data\\InfAct_plus\\InfAct_plus_{N_FEATS}d_yt_split.pkl", 'val', window_size=60, random_selection="uniform_choose")
     val_dataset = Feeder(DATA_PATH, 'val', window_size=60, random_selection="uniform_choose", break_samples=False)
     
 
@@ -122,19 +64,33 @@ if __name__ == "__main__":
 
     ce_loss = nn.CrossEntropyLoss(reduction='mean')
     optimizer = torch.optim.AdamW(model.parameters(), lr=BASE_LR)#SGD(weight_decay=0.01, lr=0.1, params=stgcn.parameters())
+        
     scheduler1 = LinearLR(optimizer, start_factor=0.1, total_iters=len(train_dataloader)*0.4*EPOCHS)
     scheduler2 = CosineAnnealingLR(optimizer, eta_min=0, T_max = len(train_dataloader)*0.6*EPOCHS)
-    scheduler = ChainedScheduler([scheduler1, scheduler2])
+    def compound_scheduler(epoch_num):
+        if epoch_num < int(0.4 * EPOCHS):
+            return scheduler1
+        else:
+            return scheduler2
 
-    print(f"Training for {EPOCHS} epochs")
+    train_logger = Logger('train', osp.join(WORK_DIR, 'train_log.txt'))
+    train_logger.log_message(f"Training parameters:")
+    train_logger.log_message(f"Model: {MODEL}")
+    train_logger.log_message(f"Data: {DATA_PATH}")
+    train_logger.log_message(f"Base learning rate: {BASE_LR}")
+    train_logger.log_message(f"Repeat: {REPEAT}")
+    train_logger.log_message(f"Training for {EPOCHS} epochs")
     best_val_acc = -1
-    for epoch in range(EPOCHS):
-        train_accuracy = do_epoch('train', epoch, model, train_dataloader)
-        val_accuracy = do_epoch('val', epoch, model, val_dataloader)
 
-        save_weights(model, f"epoch_{epoch+1:03d}.pth")
+    epoch_runner = EpochRunner(model, device, train_logger, optimizer, compound_scheduler, ce_loss, EPOCHS)
+    for epoch in range(EPOCHS):
+        train_accuracy = epoch_runner.run_epoch('train', epoch, train_dataloader)
+        val_accuracy, _, _, _ = epoch_runner.run_epoch('val', epoch, val_dataloader)
+
+        save_weights(model, f"epoch_{epoch+1:03d}.pth", WORK_DIR)
         if val_accuracy>best_val_acc:
             save_weights(model, 
-                              f"best_val_results_epoch_{epoch+1:03d}_acc_{val_accuracy:.2f}.pth",
-                              "best_val_results")
+                         f"best_val_results_epoch_{epoch+1:03d}_acc_{val_accuracy:.2f}.pth",
+                         WORK_DIR,
+                         "best_val_results")
             best_val_acc = val_accuracy
